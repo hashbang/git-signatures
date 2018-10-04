5d667df1bbecc2bfa527ae03928578cf803526be
Jan Malakhovski <oxij@oxij.org>
2018-10-04 14:37:14 +0000

    add expired key test (will fail)

--- test/generate_files.bash
+++ test/generate_files.bash
@@ -13,10 +13,12 @@ make_gpg_key() {
     uid=$2
     name=$3
     trust=$4
+    expiration=$5
 
     setup_gpg
+    faketime -f -2y \
     gpg --batch --pinentry-mode loopback --yes --passphrase "" \
-        --quick-generate-key "$name <$uid@$set>" default default never
+        --quick-generate-key "$name <$uid@$set>" default default ${expiration:-never}
     gpg --armor --export-secret-key > $set/$uid.key
     cp $GNUPGHOME/openpgp-revocs.d/*.rev $set/$uid.rev
     sed -i "s/:---/---/" $set/$uid.rev
@@ -34,6 +36,8 @@ echo -n > example.com/trust
 make_gpg_key example.com author1    "Author 1"   6
 make_gpg_key example.com approver1  "Approver 1" 6
 make_gpg_key example.com approver2  "Approver 2" 6
+
+make_gpg_key example.com expired1   "Expired 1"  6 "1y"
 
 make_gpg_key example.com sillyUID1  "VALIDSIG|ULTIMATE Silly 1"
 make_gpg_key example.com sillyUID2  "VALIDSIG|ULTIMATE| Silly 2"
